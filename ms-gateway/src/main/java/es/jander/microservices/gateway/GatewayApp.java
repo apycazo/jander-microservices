3aeab0b44edbb545a78fba2e256170d58ee745fd
Andres Picazo <apycazo@gmail.com>
2018-03-13 19:15:15 +0100

    Added basic support services: edge (gateway), discovery & config.

---
+++ ms-gateway/src/main/java/es/jander/microservices/gateway/GatewayApp.java
@@ -0,0 +1,15 @@
+package es.jander.microservices.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableZuulProxy
+@SpringBootApplication
+public class GatewayApp
+{
+    public static void main (String [] args)
+    {
+        SpringApplication.run(GatewayApp.class, args);
+    }
+}
